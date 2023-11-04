4aa4f4084a88f12bc06cb4e2c677e479f66bfecb
David Inoa <davidinoa@gmail.com>
2023-11-04 02:13:38 -0400

    Implement component for selecting a font for the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import FontSelect from './FontSelect'
 import Logo from './assets/images/logo.svg?react'
 
 export default function App() {
   return (
     <div>
-      <header className="flex items-center gap-2">
+      <header className="flex items-center gap-4">
         <div className="flex-grow">
-          <Logo />
+          <Logo className="h-9" />
         </div>
-        <div>font select</div>
+        <FontSelect />
+        <div className="h-8 w-1 border-r"></div>
         <div>color scheme</div>
       </header>
       <h1 className="font-serif text-3xl font-bold text-black underline">
---
+++ src/FontSelect.tsx
@@ -0,0 +1,40 @@
+import { Fragment, useState } from 'react'
+import IconArrowDown from './assets/images/icon-arrow-down.svg?react'
+import { Listbox } from '@headlessui/react'
+
+const fontOptions = [
+  { id: 'sansSerif', name: 'Sans Serif', className: 'font-sans' },
+  { id: 'serif', name: 'Serif', className: 'font-serif' },
+  { id: 'mono', name: 'Mono', className: 'font-mono' },
+]
+
+export default function FontSelect() {
+  const [selectedFont, setSelectedFont] = useState(fontOptions[0])
+
+  return (
+    <div className="relative gap-4 text-sm font-bold">
+      <Listbox value={selectedFont} onChange={setSelectedFont}>
+        <Listbox.Button className="flex items-center gap-3">
+          {selectedFont.name}
+          <IconArrowDown className="stroke-lavender" />
+        </Listbox.Button>
+        <Listbox.Options className="absolute right-0 top-8 flex min-w-[180px] flex-col gap-4 rounded-3xl bg-white p-6 shadow-xl">
+          {fontOptions.map((font) => (
+            <Listbox.Option key={font.id} value={font} as={Fragment}>
+              {({ selected }) => (
+                <li
+                  className={`${font.className} ${
+                    (selected || selectedFont.name === font.name) &&
+                    'text-lavender'
+                  }`}
+                >
+                  {font.name}
+                </li>
+              )}
+            </Listbox.Option>
+          ))}
+        </Listbox.Options>
+      </Listbox>
+    </div>
+  )
+}
