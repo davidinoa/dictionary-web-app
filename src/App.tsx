30c9158f8c973671cc3c77e1b0c5c5b340144bfd
David Inoa <davidinoa@gmail.com>
2023-11-04 01:07:06 -0400

    Render the logo in the header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import Logo from './assets/images/logo.svg?react'
+
 export default function App() {
   return (
-    <h1 className="font-serif text-3xl font-bold underline text-black">
-      Hello world!
-    </h1>
+    <div>
+      <header className="flex items-center gap-2">
+        <div className="flex-grow">
+          <Logo />
+        </div>
+        <div>font select</div>
+        <div>color scheme</div>
+      </header>
+      <h1 className="font-serif text-3xl font-bold text-black underline">
+        Hello world!
+      </h1>
+    </div>
   )
 }
